4b0628a5917f6d66f85965a624af72960fafa861
Sarbeh <handoyonoterakhir@gmail.com>
2023-05-05 02:42:42 +0700

    tes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import UnoCSS from 'unocss/astro';
+import robotsTxt from "astro-robots-txt";
+import compressor from "astro-compressor";
+import mdx from "@astrojs/mdx";
+import alpinejs from "@astrojs/alpinejs";
+import vercel from "@astrojs/vercel/serverless";
+import auth from "auth-astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), UnoCSS(), robotsTxt(),  mdx(), alpinejs(), auth(),compressor(),],
+  output: "server",
+  adapter: vercel()
+});
